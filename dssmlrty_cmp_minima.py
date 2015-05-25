90a089b774915674436acb7b7dd0eb5c3a221ddd
ElGamacy <gamacy@pharm.asu.edu.eg>
2015-05-25 16:04:56 +0200

    Update dssmlrty_cmp_minima.py

--- dssmlrty_cmp_minima.py
+++ dssmlrty_cmp_minima.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 #import scipy.spatial.distance as dist
 
-##########################
-# atom records extractor #
+#############
+# usage doc #
 ######################################################################
 def usage():
 
